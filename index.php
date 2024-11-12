37d3d122fc182510900dd5d44ed38944089c1121
Blandine NAHORNY <150047129+BlandineNHY@users.noreply.github.com>
2024-11-12 15:06:58 +0100

    feat : Start wordpress theme

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: Index */
+get_header();
+?>
+
+<div id="accueil">
+    <!-- Contenu de l'accueil -->
+</div>
+
+<div id="a-propos">
+    <!-- Contenu de la section À propos -->
+</div>
+
+<div id="formations">
+    <!-- Contenu de la section Formations -->
+</div>
+
+<div id="competences">
+    <!-- Contenu de la section Compétences -->
+</div>
+
+<div id="projets">
+    <!-- Contenu de la section Projets -->
+</div>
+
+<div id="contact">
+    <!-- Contenu de la section Contact -->
+</div>
+
+<?php get_footer(); ?>
